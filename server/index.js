5818d09a066de4a5ef3c69037384ccdb0f0c2b1a
LauraMFerrisNY <lauramferrisny@gmail.com>
2025-04-23 20:46:48 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -48,6 +48,12 @@ const {
 const express = require('express');
 const { seedDatabases } = require('./dbSeed');
 const cors = require('cors');
+
+const path = require('path');
+const { fileURLToPath } = require('url');
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 const app = express();
 app.use(express.json());
 app.use(cors());
