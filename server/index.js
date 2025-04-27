eabded55ee6cc864a8cd2e439f7d03d96d23e718
LauraMFerrisNY <LauraMFerrisNY@gmail.com>
2025-04-27 19:02:51 -0400

    Fixed render issue again x2

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "dist")));
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.use(cors());
 //app.options('*', cors());
