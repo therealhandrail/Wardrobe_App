2552d7ce017193364ed0b4edea726ac2c861ccb8
LauraMFerrisNY <LauraMFerrisNY@gmail.com>
2025-04-27 18:59:19 -0400

    Fixed render issue again

--- server/index.js
+++ server/index.js
@@ -43,7 +43,10 @@ const cors = require('cors');
 const path = require('path');
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "dist", "index.html")));
+app.use(express.static(path.join(__dirname, "dist")));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 app.use(cors());
 //app.options('*', cors());
 
