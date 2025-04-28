3dc0b61dba9760776ac94de1d023be9b9ffc246e
LauraMFerrisNY <lauramferrisny@gmail.com>
2025-04-27 20:15:05 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,6 +45,7 @@ const app = express();
 app.use(express.json());
 //app.use(express.static(path.join(__dirname, "dist/index.html")));
 app.get('/', (req, res)=> res.sendFile(path.join(__dirname, '../dist/index.html')));
+app.get('/MyClothing', (req, res)=> res.sendFile(path.join(__dirname, '../dist/index.html')));
 app.use('/assets', express.static(path.join(__dirname, '../dist/assets')));
 app.use(cors());
 //app.options('*', cors());
